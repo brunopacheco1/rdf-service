dfa510c3265c00683043ab6f1a96663d532c2212
Bruno Pacheco <bruno.pacheco@lnds.lu>
2023-09-01 07:19:27 +0200

    Add Jena and Fuseki

--- src/main/java/io/github/brunopacheco1/rdfservice/RdfServiceApplication.java
+++ src/main/java/io/github/brunopacheco1/rdfservice/RdfServiceApplication.java
@@ -1,13 +1,25 @@
 package io.github.brunopacheco1.rdfservice;
 
+import org.apache.jena.fuseki.main.FusekiServer;
+import org.apache.jena.query.DatasetFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RdfServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RdfServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RdfServiceApplication.class, args);
+    }
 
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    FusekiServer fusekiServer() {
+        var dataset = DatasetFactory.createTxnMem();
+        var server = FusekiServer.create()
+                .add("/ds", dataset)
+                .port(3000)
+                .build();
+        return server;
+    }
 }
