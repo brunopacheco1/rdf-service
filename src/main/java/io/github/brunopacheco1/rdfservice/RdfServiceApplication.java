e7d988a883f22318c0cc7617b617a9371dfa9433
Bruno Pacheco <bruno.pacheco@lnds.lu>
2023-09-01 06:24:07 +0200

    Add Spring Boot

---
+++ src/main/java/io/github/brunopacheco1/rdfservice/RdfServiceApplication.java
@@ -0,0 +1,13 @@
+package io.github.brunopacheco1.rdfservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RdfServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RdfServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/brunopacheco1/rdfservice/RdfServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.brunopacheco1.rdfservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RdfServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
